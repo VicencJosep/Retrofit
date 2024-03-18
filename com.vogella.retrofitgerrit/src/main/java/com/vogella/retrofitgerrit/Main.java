d9a1b71bf58e24cbe08a5f9e79ca0ecea2abb183
Vicenç Josep Peñalver Navarro <vicenc.josep.penalver@estudiantat.upc.edu>
2024-03-18 20:10:46 +0100

    commit inical

--- com.vogella.retrofitgerrit/src/main/java/com/vogella/retrofitgerrit/Main.java
+++ com.vogella.retrofitgerrit/src/main/java/com/vogella/retrofitgerrit/Main.java
@@ -1,8 +1,20 @@
 package com.vogella.retrofitgerrit;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.http.GET;
+
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.start();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.example.com")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        // @GET("users")
+        // Observable<List<User>> getUsers();
     }
 }
